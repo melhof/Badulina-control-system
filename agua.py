e975be86fea77b1b6b8c6cc91a92e3eb74cfda75
Taylor <tay333@gmail.com>
2018-07-23 17:09:07 +0200

    init

---
+++ agua.py
@@ -0,0 +1,20 @@
+
+from time import sleep
+from widgetlords.pi_spi_din import *
+
+init()
+relays = Mod4KO(ChipEnable.CE3)
+
+def tick():
+    relays.write(0x1)
+    sleep(1)
+    relays.write(0x0)
+    sleep(1)
+    
+def main():
+    while True:
+        tick()
+
+
+if __name__ == '__main__':
+    main()
