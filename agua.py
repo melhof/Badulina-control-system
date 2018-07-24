290ae482c679dd30bdcce6cd769957e2ee1db717
Taylor Lee <tay333@gmail.com>
2018-07-24 17:46:36 +0200

    needs testing

--- agua.py
+++ agua.py
@@ -1,19 +1,22 @@
 #! /usr/bin/python3
+
 from time import sleep
-from widgetlords.pi_spi_din import *
 
-init()
-relays = Mod4KO(ChipEnable.CE3)
+from drivers import KMTronic, Mod4KO
 
-def tick():
-    relays.write(0x1)
-    sleep(1)
-    relays.write(0x0)
-    sleep(1)
-    
+
+def tick(board):
+    for idx in range(board.size):
+        board.on(elem)
+        time.sleep(1)
+        board.off(elem)
+
 def main():
+    kmt = KMTronic()
+    mod4 = Mod4KO()
     while True:
-        tick()
+        tick(kmt)
+        tick(mod4)
 
 
 if __name__ == '__main__':
