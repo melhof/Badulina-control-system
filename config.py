43e1db7e2c66d986cc92c6194155ae9c2547d708
Taylor Lee <tay333@gmail.com>
2018-08-10 18:52:50 +0200

    actually needed ip!

--- config.py
+++ config.py
@@ -3,13 +3,16 @@ import os
 import getpass
 
 PI = getpass.getuser() == 'pi'
-SESSION_COOKIE_DOMAIN = False #avoid setting to SERVER_NAME (which is an IP)
+PI_IP = os.environ['PI_IP']
+
+#avoid setting to SERVER_NAME (which is an IP)
+SESSION_COOKIE_DOMAIN = False
 
 if PI:
     SECRET_KEY = os.environ['FLASK_SECRET_KEY']
     ENV = 'production'
     DEBUG = False
-    SERVER_NAME = '0.0.0.0:5000'
+    SERVER_NAME = '{}:5000'.format(PI_IP)
 else:
     ENV = 'development'
     DEBUG = True
@@ -18,4 +21,3 @@ else:
 SQLALCHEMY_DATABASE_URI = 'sqlite:///db.sqlite'
 SQLALCHEMY_TRACK_MODIFICATIONS = True # not really needed, just explicitly set to avoid warnings
 
-PI_IP = os.environ['PI_IP']
